636029028f5e2e2bbb9964935a2ab164071b05fb
iusztinpaul <p.e.iusztin@gmail.com>
2024-04-01 15:41:33 +0300

    feat: Extend user functionality

---
+++ course/module-1/lib.py
@@ -0,0 +1,16 @@
+from errors import ImproperlyConfigured
+
+
+def user_to_names(user: str | None) -> tuple[str, str]:
+    if user is None:
+        raise ImproperlyConfigured("User name is empty")
+
+    name_tokens = user.split(" ")
+    if len(name_tokens) == 0:
+        raise ImproperlyConfigured("User name is empty")
+    elif len(name_tokens) == 1:
+        first_name, last_name = name_tokens[0], name_tokens[0]
+    else:
+        first_name, last_name = " ".join(name_tokens[:-1]), name_tokens[-1]
+
+    return first_name, last_name
