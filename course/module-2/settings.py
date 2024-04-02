2879424eea342650c393af59862bc2e9742b6011
Vesa Alexandru <vesaalexandru95@gmail.com>
2024-04-02 06:12:04 +0300

    ready tot test

--- course/module-2/settings.py
+++ course/module-2/settings.py
@@ -7,5 +7,10 @@ class AppSettings(BaseSettings):
     MONGO_DATABASE_HOST: str = "mongodb://127.0.0.1:27017/?replicaSet=rs0"
     MONGO_DATABASE_NAME: str = "scrabble"
 
+    RABBITMQ_HOST: str = "localhost"  # or the Docker host if running remotely
+    RABBITMQ_PORT: int = 5673  # Port mapped in Docker Compose
+    RABBITMQ_DEFAULT_USERNAME: str = "guest"  # Default username
+    RABBITMQ_DEFAULT_PASSWORD: str = "guest"  # Default password
+
 
 settings = AppSettings()
