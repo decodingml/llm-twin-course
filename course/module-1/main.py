896b23fcc2ed9a9133f1fc140b4af1a5e8bddec7
iusztinpaul <p.e.iusztin@gmail.com>
2024-04-13 09:50:52 +0300

    Ruff format

--- course/module-1/main.py
+++ course/module-1/main.py
@@ -17,7 +17,7 @@ _dispatcher.register("github", GithubCrawler)
 
 def handler(event, context: LambdaContext) -> dict[str, Any]:
     first_name, last_name = lib.user_to_names(event.get("user"))
-    
+
     user = UserDocument.get_or_create(first_name=first_name, last_name=last_name)
 
     link = event.get("link")
