e3a368dbd1a387123717b0708f7ec985ca8ddfa4
Vesa Alexandru <vesaalexandru95@gmail.com>
2024-04-22 06:36:16 +0200

    cleaning

--- course/module-4/llm_components/chain.py
+++
@@ -1,8 +0,0 @@
-from langchain.chains.llm import LLMChain
-from langchain.prompts import PromptTemplate
-
-
-class GeneralChain:
-    @staticmethod
-    def get_chain(llm, template: PromptTemplate, output_key: str, verbose=True):
-        return LLMChain(llm=llm, prompt=template, output_key=output_key, verbose=verbose)
