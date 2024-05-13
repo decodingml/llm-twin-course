2e0c5e3054234d8cfd36e156930e4ea9895fefd4
Anca <ancamuscalagiu1@gmail.com>
2024-05-13 21:49:20 +0300

    refactoring model

--- course/module-4/finetuning/model.py
+++ course/module-4/finetuning/model.py
@@ -1,16 +1,16 @@
 import logging
-
 import pandas as pd
 import qwak
+import yaml
 from datasets import load_dataset, DatasetDict
-from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model, PeftModel
 from qwak.model.adapters import DefaultOutputAdapter
 from qwak.model.base import QwakModel
 import torch as th
 from qwak.model.schema import ModelSchema
 from qwak.model.schema_entities import RequestInput, InferenceOutput
 from transformers import AutoTokenizer, DataCollatorForLanguageModeling, TrainingArguments, Trainer, BitsAndBytesConfig, \
-    AutoModelForCausalLM
+    AutoModelForCausalLM, PreTrainedModel
 from comet_ml import Experiment
 from comet_ml.integration.pytorch import log_model
 import os
@@ -20,13 +20,13 @@ from settings import settings
 
 class CopywriterModel(QwakModel):
     def __init__(self, is_saved: bool = False, train_data_file: str = "./linkedin-train.json",
-                 validation_data_file: str = "./linkedin-validation.json", model_save_dir: str = "./model"):
+                 validation_data_file: str = "./linkedin-validation.json", model_save_dir: str = "./model",
+                 model_type: str = "mistralai/Mistral-7B-Instruct-v0.1"):
         self._prep_environment()
-        self.OPENAI_API_KEY = settings.OPENAI_API_KEY
         self.experiment = None
         self.data_files = {"train": train_data_file, "validation": validation_data_file}
         self.model_save_dir = model_save_dir
-        self.model_type = "mistralai/Mistral-7B-Instruct-v0.1"
+        self.model_type = model_type
         if is_saved:
             self.experiment = Experiment(
                 api_key=settings.COMET_API_KEY,
@@ -42,16 +42,16 @@ class CopywriterModel(QwakModel):
     def init_model(self):
         self.model = AutoModelForCausalLM.from_pretrained(
             self.model_type,
-            token=self.OPENAI_API_KEY,
+            token=settings.HUGGINGFACE_ACCESS_TOKEN,
             device_map=th.cuda.current_device(),
             quantization_config=self.nf4_config,
             use_cache=False,
             torchscript=True
         )
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_type, token=self.OPENAI_API_KEY)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_type, token=settings.HUGGINGFACE_ACCESS_TOKEN)
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.tokenizer.padding_side = "right"
-        logging.info("Initialized model successfully!")
+        logging.info(f'Initialized model{self.model_type} successfully')
 
     def _init_4bit_config(self):
         self.nf4_config = BitsAndBytesConfig(
@@ -64,7 +64,7 @@ class CopywriterModel(QwakModel):
             self.experiment.log_parameters(self.nf4_config)
         logging.info("Initialized config for param representation on 4bits successfully!")
 
-    def _init_qlora_config(self):
+    def _initialize_qlora(self, model: PreTrainedModel) -> PeftModel:
         self.qlora_config = LoraConfig(
             lora_alpha=16,
             lora_dropout=0.1,
@@ -76,41 +76,31 @@ class CopywriterModel(QwakModel):
         if self.experiment:
             self.experiment.log_parameters(self.qlora_config)
 
-        self.model = prepare_model_for_kbit_training(self.model)
-        self.model = get_peft_model(self.model, self.qlora_config)
-        if self.experiment:
-            log_model(self.experiment, model=self.model, model_name="Copywriter-v1")
+        model = prepare_model_for_kbit_training(model)
+        model = get_peft_model(model, self.qlora_config)
         logging.info("Initialized qlora config successfully!")
+        return model
 
     def _init_trainig_args(self):
-        self.training_arguments = TrainingArguments(
-            output_dir="mistral_instruct_generation",
-            max_steps=10,
-            per_device_train_batch_size=1,
-            logging_steps=10,
-            save_strategy="epoch",
-            evaluation_strategy="steps",
-            eval_steps=2,
-            learning_rate=2e-4,
-            fp16=True,
-            remove_unused_columns=False,
-            lr_scheduler_type='constant',
-        )
+        with open('config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
+        self.training_arguments = TrainingArguments(**config['training_arguments'])
         if self.experiment:
             self.experiment.log_parameters(self.training_arguments)
         logging.info("Initialized training arguments successfully!")
 
-    def _remove_class_attributes(self):
+    def _remove_model_class_attributes(self):
+        # remove needed in order to skip default serialization with Pickle done by Qwak
         del self.model
-        del self.trained_model
+        del self.trainer
 
-    def generate_prompt(self, sample):
+    def generate_prompt(self, sample: dict) -> dict:
         full_prompt = f"""<s>[INST]{sample['instruction']}
         [/INST] {sample['content']}</s>"""
         result = self.tokenize(full_prompt)
         return result
 
-    def tokenize(self, prompt):
+    def tokenize(self, prompt: str) -> dict:
         result = self.tokenizer(
             prompt,
             padding="max_length",
@@ -120,7 +110,7 @@ class CopywriterModel(QwakModel):
         result["labels"] = result["input_ids"].copy()
         return result
 
-    def load_dataset(self):
+    def load_dataset(self) -> DatasetDict:
         raw_datasets = load_dataset("json", data_files=self.data_files)
         train_data = raw_datasets['train']
         val_data = raw_datasets['validation']
@@ -136,32 +126,32 @@ class CopywriterModel(QwakModel):
     def build(self):
         self._init_4bit_config()
         self.init_model()
-        self.model = prepare_model_for_kbit_training(self.model)
         if self.experiment:
             self.experiment.log_parameters(self.nf4_config)
-        self._init_qlora_config()
+        self.model = self._initialize_qlora(self.model)
         self._init_trainig_args()
         tokenized_datasets = self.load_dataset()
         self.device = th.device("cuda" if th.cuda.is_available() else "cpu")
         self.model = self.model.to(self.device)
-        self.trained_model = Trainer(
+        self.trainer = Trainer(
             model=self.model,
             args=self.training_arguments,
             train_dataset=tokenized_datasets['train'],
             eval_dataset=tokenized_datasets['validation'],
             tokenizer=self.tokenizer,
-            data_collator=DataCollatorForLanguageModeling(self.tokenizer, mlm=False),
         )
         logging.info("Initialized model trainer")
-        self.trained_model.train()
+        self.trainer.train()
+        if self.experiment:
+            log_model(self.experiment, model=self.model, model_name="Copywriter")
         logging.info("Finished model finetuning!")
-        self.trained_model.save_model(self.model_save_dir)
+        self.trainer.save_model(self.model_save_dir)
         logging.info(f'Finished saving model to {self.model_save_dir}')
-        self._remove_class_attributes()
+        self._remove_model_class_attributes()
         logging.info("Finished removing model class attributes!")
 
     def initialize_model(self):
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_save_dir, token=self.OPENAI_API_KEY,
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_save_dir, token=settings.HUGGINGFACE_ACCESS_TOKEN,
                                                           quantization_config=self.nf4_config)
         logging.info(f'Successfully loaded model from {self.model_save_dir}')
 
