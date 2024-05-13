17ffc9cc2cce27a00c9cbe6094df7e678b0d30d8
Anca <ancamuscalagiu1@gmail.com>
2024-05-13 21:49:20 +0300

    refactoring model

--- course/module-4/settings.py
+++ course/module-4/settings.py
@@ -5,7 +5,7 @@ class AppSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     TOKENIZERS_PARALLELISM: str = "false"
-    OPENAI_API_KEY: str = ""
+    HUGGINGFACE_ACCESS_TOKEN: str = ""
     COMET_API_KEY: str = ""
     COMET_WORKSPACE: str = ""
     COMET_PROJECT: str = ""
