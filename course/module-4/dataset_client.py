77bed8fd9625a515352905394901971da3e4f40d
Anca <ancamuscalagiu1@gmail.com>
2024-05-14 19:34:47 +0300

    refactorings

--- course/module-4/dataset_client.py
+++ course/module-4/dataset_client.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from settings import settings
 
 
-class DatasetHandler:
+class DatasetClient:
     def __init__(self, output_dir: str = "./finetuning"):
         self.project = settings.COMET_PROJECT
         self.api_key = settings.COMET_API_KEY
@@ -25,27 +25,29 @@ class DatasetHandler:
         except Exception as e:
             logging.error(f"Error retrieving artifact: {str(e)}")
 
-    def split_data(self, artifact_name: str):
+    def split_data(self, artifact_name: str) -> tuple:
         try:
+            training_file_path = os.path.join(self.output_dir, 'train.json')
+            validation_file_path = os.path.join(self.output_dir, 'validation.json')
             file_name = artifact_name + ".json"
             with open(os.path.join(self.output_dir,file_name), 'r') as file:
                 data = json.load(file)
 
             train_data, val_data = train_test_split(data, test_size=0.2, random_state=42)
 
-            with open(os.path.join(self.output_dir, 'train.json'), 'w') as train_file:
+            with open(training_file_path, 'w') as train_file:
                 json.dump(train_data, train_file)
 
-            with open(os.path.join(self.output_dir, 'validation.json'), 'w') as val_file:
+            with open(validation_file_path, 'w') as val_file:
                 json.dump(val_data, val_file)
 
             logging.info("Data split into train.json and validation.json successfully.")
-
+            return training_file_path, validation_file_path
         except Exception as e:
             logging.error(f"Error splitting data: {str(e)}")
 
     def download_dataset(self, file_name: str):
         self.get_artifact(file_name)
-        self.split_data(file_name)
+        return self.split_data(file_name)
 
 
