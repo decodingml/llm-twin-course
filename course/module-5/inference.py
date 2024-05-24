41a0fbce7a84ee2b2d26ac4866a3bc7c444013f3
razvantalex <alexandrurazvant@gmail.com>
2024-05-24 21:41:46 +0300

    fix: Added GPT3.5 specific LLM evaluation flow

--- course/module-5/inference.py
+++ course/module-5/inference.py
@@ -1,51 +1,53 @@
 import pandas as pd
-from qwak_inference import RealTimeClient
-
-from rag.retriever import VectorRetriever
-from model_evaluation.prompt_monitor import PromptMonitor
 from llm_components.prompt_templates import InferenceTemplateV1
-from model_evaluation.evaluation import evaluate
-
+from model_evaluation.evaluation import rag_eval_using_GPT
+from model_evaluation.prompt_monitor import PromptMonitor
+from qwak_inference import RealTimeClient
+from rag.retriever import VectorRetriever
 from settings import settings
 
 
 class ModelInference:
 
     def __init__(self):
-        self.qwak_client = RealTimeClient(model_id=settings.MODEL_ID,
-                                          model_api=settings.MODEL_API)
+        self.qwak_client = RealTimeClient(
+            model_id=settings.MODEL_ID, model_api=settings.MODEL_API
+        )
         self.template = InferenceTemplateV1()
         self.prompt_monitor = PromptMonitor()
 
+    def infer_only(self, query: str) -> dict:
+
+        template = self.template.create_template()
+        prompt = template.format(question=query)
+
+        input_ = pd.DataFrame([{"instruction": prompt.strip()}]).to_json()
+
+        response = self.qwak_client.predict(input_)
+
+        return {"content": response}
+
     def generate_content(self, query: str) -> dict:
         retriever = VectorRetriever(query=query)
-        hits = retriever.retrieve_top_k(k=settings.TOP_K, to_expand_to_n_queries=settings.EXPAND_N_QUERY)
+        hits = retriever.retrieve_top_k(
+            k=settings.TOP_K, to_expand_to_n_queries=settings.EXPAND_N_QUERY
+        )
         context = retriever.rerank(hits=hits, keep_top_k=settings.KEEP_TOP_K)
 
         template = self.template.create_template()
-        prompt = template.format(question=query,
-                                 context=context)
+        prompt = template.format(question=query, context=context)
 
-        input_ = pd.DataFrame(
-            [
-                {
-                    'instruction': prompt
-                }
-            ]
-        ).to_json()
+        input_ = pd.DataFrame([{"instruction": prompt}]).to_json()
 
         response = self.qwak_client.predict(input_)
-        evaluation = evaluate(query=query,
-                              context=context,
-                              output=str(response))
+        evaluation = rag_eval_using_GPT(
+            query=query, context=context, output=str(response)
+        )
 
         self.prompt_monitor.log_prompt(
             prompt=prompt,
-            prompt_template_variables={'question': query, 'context': context},
-            output=response
+            prompt_template_variables={"question": query, "context": context},
+            output=response,
         )
 
-        return {
-            'content': response,
-            'evaluation': evaluation
-        }
+        return {"content": response, "evaluation": evaluation}
--- course/module-5/main.py
+++ course/module-5/main.py
@@ -1,14 +1,20 @@
-from inference import ModelInference
+import model_evaluation.evaluation as eval
 from dotenv import load_dotenv
-if __name__ == '__main__':
+from inference import ModelInference
+from model_evaluation.prompt_monitor import PromptMonitor
+
+if __name__ == "__main__":
     load_dotenv()
     tool = ModelInference()
-    query = """
-            Hello my author_id is 1.
+    llm_monitor = PromptMonitor()
+    query = """Could you please draft a LinkedIn post discussing Vector Databases? I'm particularly interested in how do they work."""
+    [content] = tool.infer_only(query=query)["content"]
 
-            Could you please draft a LinkedIn post discussing RAG systems?
-            I'm particularly interested in how RAG works and how it is integrated with vector DBs and large language models (LLMs).
-            """
-    content = tool.generate_content(query=query)
+    result = eval.llm_eval_using_GPT(query=query, output=content)
+    print(result)
+    llm_monitor.log_prompt(
+        prompt=result, prompt_template_variables={"query": query}, output=content
+    )
+
     for item in content:
         print(item)
